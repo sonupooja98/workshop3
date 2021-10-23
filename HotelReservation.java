88fe2c3010ad07f2fd4afe9f0d24573c8c1fc6a1
sonupooja <sonupooja2398@gmail.com>
2021-10-23 09:05:48 +0530

    welcome hotel

---
+++ HotelReservation.java
@@ -0,0 +1,63 @@
+package com.bridgelabzworkshop3;
+import java.util.*;
+class Hotel{
+    public String hotelName;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getHotelPrice() {
+        return hotelPrice;
+    }
+
+    public void setHotelPrice(int hotelPrice) {
+        this.hotelPrice = hotelPrice;
+    }
+
+    public  int hotelPrice;
+
+    public Hotel(String hotelName, int hotelPrice) {
+        this.hotelName = hotelName;
+        this.hotelPrice = hotelPrice;
+    }
+
+    public String toString(){
+        return "Hotel Name: "+hotelName +"Hotel Price: "+hotelPrice;
+    }
+}
+public class HotelReservation {
+	public ArrayList<Hotel> hotelDetails;
+
+    public HotelReservation() {
+        hotelDetails = new ArrayList<>();
+    }
+
+    public void addHotel(String hotelName, Integer hotelPrice) {
+        Hotel adder = new Hotel(hotelName, hotelPrice);
+        hotelDetails.add(adder);
+    }
+
+    public void getHotelDetails() {
+        String hotelName1 = "LakeWood";
+        String hotelName2 = "BridgeWood";
+        String hotelName3 = "RidgeWood";
+        int lackWoodPrice1 = 100;
+        int bridgeWoodPrice1 = 160;
+        int ridgeWoodPrice1 = 220;
+
+        addHotel(hotelName1, lackWoodPrice1);
+        addHotel(hotelName2, bridgeWoodPrice1);
+        addHotel(hotelName3, ridgeWoodPrice1);
+    }
+
+    public void viewHotels() {
+        getHotelDetails();
+        System.out.println(hotelDetails);
+    }
+}
+
---
+++ HotelReservationMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabzworkshop3;
+import java.util.*;
+public class HotelReservationMain {
+	 public static void main(String[] args) {
+            System.out.println("welcome to hotel reservation system");
+	        HotelReservation hotelReservation = new HotelReservation();
+	        hotelReservation.viewHotels();
+
+	    }
+	}
+
